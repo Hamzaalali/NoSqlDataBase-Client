63db87287629979efd784542af89ce50dbe459c5
hamza alali <hamza.tech.alali@gmail.com>
2022-11-19 21:34:09 +0300

    finisht

--- src/main/java/org/example/bootstrapper/BootstrapperClient.java
+++ src/main/java/org/example/bootstrapper/BootstrapperClient.java
@@ -2,6 +2,7 @@ package org.example.bootstrapper;
 import org.example.client.QueryManager;
 import org.example.server_client.ServerClientCommunicator;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -19,14 +20,23 @@ public class BootstrapperClient {
             throw new RuntimeException(e);
         }
     }
-    public void register(String username,String password){
+    public Long register(String username,String password){//returns the tcp port that the user can connect to
         try{
             JSONObject request=new JSONObject();
             request.put("username",username);
             request.put("password",password);
             request.put("requestType",BootstrapperRequests.REGISTER.toString());
             ServerClientCommunicator.sendJson(socket,request);
+            JSONObject messageFromServer=ServerClientCommunicator.readJson(socket);
+            if(((Long)messageFromServer.get("code_number"))==1){
+                throw new RuntimeException((String) messageFromServer.get("error_message"));
+            }
+            return (Long)messageFromServer.get("tcpPort");
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
